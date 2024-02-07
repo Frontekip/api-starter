b4d806d0e253ac340d331ab739b15f267faeb30a
Yaşar İÇLİ <yasaricli@gmail.com>
2024-02-07 10:27:26 +0300

    Update api.js

--- imports/utils/api.js
+++ imports/utils/api.js
@@ -2,30 +2,7 @@ import { Users } from "../collections/users/users";
 
 const Api = new Restivus({
   version: "v1",
-  enableCors: true,
   useDefaultAuth: true,
-
-  defaultOptionsEndpoint: {
-    action() {
-      this.response.writeHead(201, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept, Z-Key, X-Auth-Token, X-User-Id",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Methods":
-          "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-      });
-
-      this.done();
-
-      return {
-        status: "success",
-        data: {
-          message: "We love OPTIONS",
-        },
-      };
-    },
-  },
 
   onLoggedIn() {
     const { user } = this;
